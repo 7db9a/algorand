179184942bd09a592e2bd4f70e7e9109a0c4c3fd
7db9a <>
2024-02-28 11:28:25 -0500

    tests(e2e): create script to compile and deploy contract and run e2e.

---
+++ algorand/tests/run_e2e.py
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Compile vote contract app.
+python examples/vote_app/vote.py
+
+# Bunde teal vote contract code for deployment
+goal clerk compile vote_clear_state.teal -d /algod/data/net1/Primary
+goal clerk compile vote_approval.teal -d /algod/data/net1/Primary
+python -m tests.integration.test_create_app
