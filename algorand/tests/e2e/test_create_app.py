89413cea0e3589ce1f84490a3d0f0da5cd812d08
7db9a <>
2024-02-28 17:59:33 -0500

    tests(e2e): update vote tests by passing optional app id.

--- algorand/tests/e2e/test_create_app.py
+++ algorand/tests/e2e/test_create_app.py
@@ -1,23 +1,31 @@
 from lib.blockchain import Vote
 
-def test_create_app(algorand):
-    app_id = algorand.create_app()
+def test_create_app(vote_app):
+    app_id = vote_app.create_app()
     assert app_id is not None and app_id > 0, "Failed to create application"
     print("Created new app-id:", app_id)
+    return app_id
     # Add calls to other test methods here
 
-def test_vote(algorand, app_args):
-    algorand.vote(app_args)
+def test_vote(vote_app, app_args):
+    vote_app.vote(app_args)
 
 
 if __name__ == "__main__":
     algod_address = "http://127.0.0.1:8080"
     algod_token = "1fa5aed7ec723da8ec9abfb6396adbbb607dd95316f8277456ec7b65afeb3893"
+    asset_id = 1653
+
+    # Creator info
     creator_mnemonic = "twin pumpkin plastic stage fortune shallow melt betray ribbon receive claim enrich price exile absent avoid woman toilet print settle shiver inform rookie absorb unaware"
-    user_mnemonic = creator_mnemonic
 
-    algorand = Vote(algod_address, algod_token, creator_mnemonic, user_mnemonic)
+    # User1 Info
+    user1_mnemonic =  "twin pumpkin plastic stage fortune shallow melt betray ribbon receive claim enrich price exile absent avoid woman toilet print settle shiver inform rookie absorb unaware"
 
-    test_create_app(algorand)
-    test_vote(algorand,[b"vote", b"choiceA"])
+    vote_app_creator = Vote(algod_address, algod_token, asset_id, creator_mnemonic, creator_mnemonic)
+    app_id = test_create_app(vote_app_creator)
+
+    vote_app_creator = Vote(algod_address, algod_token, asset_id, user1_mnemonic, user1_mnemonic, app_id)
+    test_vote(vote_app_creator, [b"vote", b"choiceA"])
+
 
