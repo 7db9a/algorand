32be1f858946195ef64625e69175c8152cba0d1a
7db9a <>
2024-02-28 13:38:47 -0500

    tests(e2e): define test runner and create app seperately.

--- algorand/tests/e2e/test_create_app.py
+++ algorand/tests/e2e/test_create_app.py
@@ -1,10 +1,14 @@
 from lib.blockchain import Vote
 
-def run_tests(algorand):
+def test_create_app(algorand):
     app_id = algorand.create_app()
     assert app_id is not None and app_id > 0, "Failed to create application"
     print("Created new app-id:", app_id)
     # Add calls to other test methods here
+
+
+def run_tests(algorand):
+    test_create_app(algorand)
 
 if __name__ == "__main__":
     algod_address = "http://127.0.0.1:8080"
