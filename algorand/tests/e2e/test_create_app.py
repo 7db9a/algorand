424969f240664ec1baf9f69ee25d13175bba3417
7db9a <>
2024-03-06 16:26:02 -0500

    test(create-app): refactor to for unnitest.

--- algorand/tests/e2e/test_create_app.py
+++ algorand/tests/e2e/test_create_app.py
@@ -1,26 +1,32 @@
 import json
+import unittest
 from lib.blockchain import Vote
 
-def test_create_app(vote_app):
-    (txid, app_id) = vote_app.create_app()
-    # After 3 million blocks, according to algorand docs, a private blockchain node will begin to stall.
-    assert app_id is not None and app_id > 0 and app_id < 3000000, "App ID should be positive and less than 3000000"
-    print("Tx ID:", txid)
-    print("Created new app-id:", app_id)
-    return app_id
-    # Add calls to other test methods here
+class TestVoteApp(unittest.TestCase):
+
+    def setUp(self):
+        # Load the configuration file
+        with open('tests/e2e/test-config.json', 'r') as file:
+            config = json.load(file)
+
+        # Accessing specific configuration data
+        creator_mnemonic = config['creatorInfo']['mnemonic']
+        algod_token = config['algodToken']
+        algod_address = config['algodAddress']
+        asset_id = config['assetId']
+
+        # Initialize the Vote instance
+        self.vote_app = Vote(algod_address, algod_token, asset_id, creator_mnemonic, creator_mnemonic)
+
+    def test_create_app(self):
+        # Use the initialized vote_app in the test
+        (txid, app_id) = self.vote_app.create_app()
+        self.assertIsNotNone(app_id, "App ID should not be None")
+        self.assertGreater(app_id, 0, "App ID should be greater than 0")
+        self.assertLess(app_id, 3000000, "App ID should be less than 3000000")
+        print("Tx ID:", txid)
+        print("Created new app-id:", app_id)
 
 if __name__ == "__main__":
-    # Load the configuration file
-    with open('tests/e2e/test-config.json', 'r') as file:
-        config = json.load(file)
+    unittest.main()
 
-    # Accessing specific configuration data
-    creator_mnemonic = config['creatorInfo']['mnemonic']
-    algod_token = config['algodToken']
-    algod_address = config['algodAddress']
-    asset_id = config['assetId']
-
-    vote_app_creator = Vote(algod_address, algod_token, asset_id, creator_mnemonic, creator_mnemonic)
-
-    test_create_app(vote_app_creator)
