78b3084d9875887b6c4a7f7bb621a4e57d714627
7db9a <>
2024-03-06 14:53:18 -0500

    tests: get user and other info from test config.

--- algorand/tests/e2e/test_create_app.py
+++ algorand/tests/e2e/test_create_app.py
@@ -1,3 +1,4 @@
+import json
 from lib.blockchain import Vote
 
 def test_create_app(vote_app):
@@ -36,18 +37,17 @@ def assert_final_state(global_state):
     assert global_state == expected_state
 
 if __name__ == "__main__":
-    algod_address = "http://127.0.0.1:8080"
-    algod_token = "1fa5aed7ec723da8ec9abfb6396adbbb607dd95316f8277456ec7b65afeb3893"
-    asset_id = 1653
+    # Load the configuration file
+    with open('tests/e2e/test-config.json', 'r') as file:
+        config = json.load(file)
 
-    # Creator Info
-    creator_mnemonic = "twin pumpkin plastic stage fortune shallow melt betray ribbon receive claim enrich price exile absent avoid woman toilet print settle shiver inform rookie absorb unaware"
-
-    # User1 Info
-    user1_mnemonic = "brain rough jazz defy absent ability jeans much hire retire metal tragic fury culture stem beach farm upset relief stove sound comic bunker able exist"
-
-    # User2 Info
-    user2_mnemonic = "mass slide nature lady river smart dismiss item cave topple place remember oxygen title travel mixture team focus live human burger knock achieve about giggle"
+    # Accessing specific configuration data
+    creator_mnemonic = config['creatorInfo']['mnemonic']
+    user1_mnemonic = config['user1Info']['mnemonic']
+    user2_mnemonic = config['user2Info']['mnemonic']
+    algod_address = config['algodAddress']
+    algod_token = config['algodToken']
+    asset_id = config['assetId']
 
     vote_app_creator = Vote(algod_address, algod_token, asset_id, creator_mnemonic, creator_mnemonic)
 
